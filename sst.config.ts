df8c21eaaa7f093fcb3feb3101109cd21ff3eddf
MadCoderr <msrfarooqi@gmail.com>
2024-01-10 00:43:06 +0500

    feat: add react frontend

--- sst.config.ts
+++ sst.config.ts
@@ -4,6 +4,7 @@ import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 
 export default {
   config(_input) {
@@ -13,6 +14,10 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+    app
+      .stack(StorageStack)
+      .stack(ApiStack)
+      .stack(AuthStack)
+      .stack(FrontendStack);
   },
 } satisfies SSTConfig;
